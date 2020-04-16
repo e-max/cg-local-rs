42613066ba11092e4f79f088b36a8a1e03269481
Max Lavrenov <max.lavrenov@optiopay.com>
2020-04-16 20:37:53 +0200

    add json

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use futures::{SinkExt, StreamExt};
 use log::{error, info};
+use serde;
+use serde::{Deserialize, Serialize};
+use serde_json;
 use std::io::{Error, ErrorKind};
 use tokio::net::{TcpListener, TcpStream};
 use tokio_tungstenite::{accept_async, WebSocketStream};
@@ -7,14 +10,35 @@ use tungstenite::protocol::Message;
 use tungstenite::Result as WsResult;
 
 const send_details: &'static str = r#"{"payload":{},"action":"send-details"}"#;
+//const send_details2: &'static str = r#"{"payload":{},"action":"SendDetails"}"#;
+const details: &'static str =
+    r#"{"action":"details","payload":{"title":"The Labyrinth","questionId":16152}}"#;
 
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(tag = "action", content = "payload")]
 enum Msg {
-    SendDetails,
+    #[serde(rename = "send-details")]
+    SendDetails {},
+    #[serde(rename = "details")]
+    Details {
+        title: String,
+        #[serde(rename = "questionId")]
+        question_id: u32,
+    },
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     println!("Hello, world!");
+
+    let m2 = Msg::Details {
+        title: "test".to_owned(),
+        question_id: 32,
+    };
+    let s2 = serde_json::to_string(&m2)?;
+    println!("s2 = {:?}", s2);
+    let m: Msg = serde_json::from_str(details)?;
+    println!("m = {:?}", m);
     let addr = "localhost:53135";
     let mut listener = TcpListener::bind(addr).await?;
     info!("Listening on: {}", addr);
