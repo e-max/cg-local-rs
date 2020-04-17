10dfb4399f02d129b29ea2230178b0ad72012e95
Max Lavrenov <max.lavrenov@optiopay.com>
2020-04-17 19:20:35 +0200

    separate input/output handlers

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,17 @@
-use futures::{join, SinkExt, Stream, StreamExt};
-use log::{error, info};
+use env_logger;
+use futures::{join, Sink, SinkExt, Stream, StreamExt};
+use log::{debug, error, info};
 use serde;
 use serde::{Deserialize, Serialize};
 use serde_json;
 use std::borrow::Cow;
 use std::io;
 use tokio::net::{TcpListener, TcpStream};
+use tokio::sync::mpsc;
 use tokio_tungstenite::{accept_async, WebSocketStream};
 use tungstenite::protocol::Message;
 use tungstenite::Error as WsError;
 use tungstenite::Result as WsResult;
-
-const send_details: &'static str = r#"{"payload":{},"action":"send-details"}"#;
-//const send_details2: &'static str = r#"{"payload":{},"action":"SendDetails"}"#;
-const details: &'static str =
-    r#"{"action":"details","payload":{"title":"The Labyrinth","questionId":16152}}"#;
 
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(tag = "action", content = "payload")]
@@ -35,16 +32,9 @@ enum Msg {
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    println!("Hello, world!");
+    env_logger::init();
+    info!("Hello, world!");
 
-    let m2 = Msg::Details {
-        title: "test".to_owned(),
-        question_id: 32,
-    };
-    let s2 = serde_json::to_string(&m2)?;
-    println!("s2 = {:?}", s2);
-    let m: Msg = serde_json::from_str(details)?;
-    println!("m = {:?}", m);
     let addr = "localhost:53135";
     let mut listener = TcpListener::bind(addr).await?;
     info!("Listening on: {}", addr);
@@ -59,29 +49,46 @@ async fn accept_connection(stream: TcpStream) -> Result<(), Error> {
     let addr = stream.peer_addr()?;
     info!("addr {}", addr);
     let mut ws_stream = accept_async(stream).await?;
+
+    let (mut writer, mut reader) = ws_stream.split();
+    let (mut tx, mut rx) = mpsc::channel::<Msg>(10);
+
     info!("Send initial message");
-    let resp = Message::Text(send_details.to_owned());
-    ws_stream.send(resp).await?;
+
+    tx.send(Msg::SendDetails {}).await?;
     info!("Start listening");
 
-    let (mut writes, mut reader) = ws_stream.split();
-
-    join!(tokio::spawn(handle_incoming(reader)));
-
-    //read.forward(write)
-    //.await
-    //.expect("Failed to forward message");
+    join!(
+        tokio::spawn(handle_incoming(tx, reader)),
+        tokio::spawn(handle_outgoing(rx, writer))
+    );
 
     Ok(())
 }
 
-async fn handle_incoming<S>(mut reader: S) -> Result<(), Error>
+async fn handle_outgoing<S>(mut rx: mpsc::Receiver<Msg>, mut writer: S) -> Result<(), Error>
+where
+    S: Sink<Message> + Unpin,
+{
+    while let Some(msg) = rx.recv().await {
+        info!("Try to send {:?}", msg);
+        let resp = serde_json::to_string(&msg)?;
+        writer
+            .send(Message::text(resp))
+            .await
+            .map_err(|e| Error::General("Some error".to_owned()))?;
+    }
+
+    Ok(())
+}
+
+async fn handle_incoming<S>(mut tx: mpsc::Sender<Msg>, mut reader: S) -> Result<(), Error>
 where
     S: Stream<Item = Result<Message, WsError>> + Unpin,
 {
     while let Some(msg) = reader.next().await {
         let msg = msg?;
-        println!("msg = {:?}", msg);
+        debug!("receive raw msg = {:?}", msg);
         match msg {
             Message::Text(txt) => handle_message(&txt)?,
             _ => (),
@@ -91,10 +98,10 @@ where
 }
 
 fn handle_message(s: &str) -> Result<(), Error> {
-    let m: Msg = serde_json::from_str(details)?;
+    let m: Msg = serde_json::from_str(s)?;
     match m {
         Msg::Details { .. } => {
-            println!(" got details = {:?}", m);
+            info!(" got Details message = {:?}", m);
         }
         _ => error!("Got unknown message {:?}", m),
     }
@@ -106,6 +113,8 @@ enum Error {
     WsError(WsError),
     JsonError(serde_json::Error),
     IO(io::Error),
+    General(String),
+    SendError(mpsc::error::SendError<Msg>),
 }
 
 impl From<WsError> for Error {
@@ -123,5 +132,11 @@ impl From<io::Error> for Error {
 impl From<serde_json::Error> for Error {
     fn from(e: serde_json::Error) -> Self {
         Error::JsonError(e)
+    }
+}
+
+impl From<mpsc::error::SendError<Msg>> for Error {
+    fn from(e: mpsc::error::SendError<Msg>) -> Self {
+        Error::SendError(e)
     }
 }
